881caf115bffe043661baa59563390f861e72eda
Ilya Buziuk <ibuziuk@redhat.com>
2017-11-08 13:11:35 +0100

    rh-che #240: Passing 'X-Request-Id' with every request to che server
    Signed-off-by: Ilya Buziuk <ibuziuk@redhat.com>

--- src/main/java/io/fabric8/che/starter/client/keycloak/KeycloakInterceptor.java
+++ src/main/java/io/fabric8/che/starter/client/keycloak/KeycloakInterceptor.java
@@ -14,6 +14,8 @@ package io.fabric8.che.starter.client.keycloak;
 
 import java.io.IOException;
 
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
@@ -22,17 +24,28 @@ import org.springframework.http.client.ClientHttpResponse;
 
 class KeycloakInterceptor implements ClientHttpRequestInterceptor {
     private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String REQUEST_ID_HEADER = "X-Request-Id";
     private String keycloakToken;
+    private String requestId;
 
-    public KeycloakInterceptor(String keycloakToken) {
+    public KeycloakInterceptor(String keycloakToken, String requestId) {
         this.keycloakToken = keycloakToken;
+        this.requestId = requestId;
     }
 
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution)
             throws IOException {
+        requestId = StringUtils.isBlank(requestId) ? generateRequestId() : requestId;
+
         HttpHeaders headers = request.getHeaders();
+        headers.add(REQUEST_ID_HEADER, requestId);
         headers.add(AUTHORIZATION_HEADER, keycloakToken);
         return execution.execute(request, body);
     }
+
+    private String generateRequestId() {
+        return RandomStringUtils.random(16, true, true).toLowerCase();
+    }
+
 }
